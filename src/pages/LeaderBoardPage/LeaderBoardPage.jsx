9022a3f211a6a5c4055b448a9ce2a19c50b1486a
AZinkevich <azinkevich@mail.ru>
2024-06-03 15:47:29 +1100

    leaderboard without style done

--- src/pages/LeaderBoardPage/LeaderBoardPage.jsx
+++ src/pages/LeaderBoardPage/LeaderBoardPage.jsx
@@ -1,10 +1,30 @@
-//import { getScore } from "../../api.js";
+import { Link } from "react-router-dom";
+import { getScores } from "../../api.js";
+import { useEffect, useState } from "react";
 
 export const LeaderBoardPage = () => {
+  const [scores, setScores] = useState([]);
+  useEffect(() => {
+    getScores().then(res => {
+      setScores(res.leaders);
+      console.log(scores);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <div>
         <h1>Это лидерборд</h1>
+      </div>
+      <Link to={"/"}>Начать игру</Link>
+      <div>
+        {scores
+          .sort((a, b) => a.time - b.time)
+          .map(score => (
+            <div>
+              {score.id} {score.name} {score.time}
+            </div>
+          ))}
       </div>
     </>
   );
